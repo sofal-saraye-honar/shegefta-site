69575b7b20907b97f7ea8bb85641cb958651242d
sofal-saraye-honar <monavarkh928@gmail.com>
2026-07-29 03:49:34 +0330

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ avatar.addEventListener("click", speak);
 
 avatar.addEventListener("dblclick", function () {
     talk("سلام دوست من. به شگفتا خوش اومدی. امیدوارم امروز چیزهای جالبی یاد بگیری.");
-});
+
 
 setInterval(speak, 8000);
 
