92c60e1d8dea4c8d7eeb36786801fed5ea081c43
sofal-saraye-honar <monavarkh928@gmail.com>
2026-08-05 01:42:03 +0330

    Update script.js

--- script.js
+++ script.js
@@ -228,3 +228,30 @@ card.style.display = "none";
 });
 
 }
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+
+topBtn.style.display = "block";
+
+}else{
+
+topBtn.style.display = "none";
+
+}
+
+};
+
+function topFunction(){
+
+window.scrollTo({
+
+top:0,
+behavior:"smooth"
+
+});
+
+}
